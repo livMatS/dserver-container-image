e59641b9d6084cdaa795b8f56878cf4f9f16587a
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-02-23 22:20:02 +0100

    ENH: Introduced env var DUMP_HTTP_REQUESTS

--- compose/production/dtool_lookup_server/wsgi.py
+++ compose/production/dtool_lookup_server/wsgi.py
@@ -1,18 +1,24 @@
+"""Create dtool_lookup_server wsgi app."""
+import os
 import pprint
 from dtool_lookup_server import create_app
 
-class LoggingMiddleware(object):
-    def __init__(self, app):
-        self._app = app
+app = create_app()
 
-    def __call__(self, env, resp):
-        errorlog = env['wsgi.errors']
-        pprint.pprint(('REQUEST', env), stream=errorlog)
+# wrap logging middleware if DUMP_HTTP_REQUESTS set true
+if os.getenv("DUMP_HTTP_REQUESTS", 'False').lower() in ('true', '1', 't'):
+    class LoggingMiddleware:
+        """Wrap wsgi app and dump all requests."""
 
-        def log_response(status, headers, *args):
-            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
-            return resp(status, headers, *args)
-        return self._app(env, log_response)
+        def __init__(self, app):
+            self._app = app
 
+        def __call__(self, env, resp):
+            errorlog = env['wsgi.errors']
+            pprint.pprint(('REQUEST', env), stream=errorlog)
 
-app = LoggingMiddleware(create_app())
+            def log_response(status, headers, *args):
+                pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
+                return resp(status, headers, *args)
+            return self._app(env, log_response)
+    app = LoggingMiddleware(app)
